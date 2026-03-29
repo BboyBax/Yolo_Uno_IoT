2cb19d385734a408587682886c7d85da518b1026
BboyBax <hts22112005@gmail.com>
2026-03-29 15:51:19 +0700

    redesign webserver and setup power-led button

---
+++ data/script.js
@@ -0,0 +1,106 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const labels = ["1h", "2h", "3h", "4h", "5h"];
+  const tempData = [27, 28, 29, 30, 29];
+  const humData = [55, 57, 60, 58, 56];
+
+  const tempCanvas = document.getElementById("tempChart");
+  const humCanvas = document.getElementById("humChart");
+
+  if (!tempCanvas || !humCanvas) {
+    console.error("Không tìm thấy canvas!");
+    return;
+  }
+
+  new Chart(tempCanvas, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "Nhiệt độ (°C)",
+        data: tempData,
+        borderColor: "red",
+        backgroundColor: "rgba(255,0,0,0.2)",
+        fill: true
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+
+  new Chart(humCanvas, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "Độ ẩm (%)",
+        data: humData,
+        borderColor: "blue",
+        backgroundColor: "rgba(0,0,255,0.2)",
+        fill: true
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+
+});
+
+// Hàm cập nhật dữ liệu cảm biến
+function updateSensors() {
+  fetch('/sensors')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('temp').innerText = data.temp + " °C";
+      document.getElementById('hum').innerText = data.hum + " %";
+    })
+    .catch(error => {
+      console.error("Lỗi khi lấy dữ liệu:", error);
+    });
+}
+
+setInterval(updateSensors, 5000);
+updateSensors();
+
+// Hàm xử lý nút nguồn
+document.getElementById("btn-power").addEventListener("click", function() {
+  let currentText = this.innerText;
+  let newState = currentText.includes("Bật") ? "on" : "off";
+
+  fetch('/power?state=' + newState)
+    .then(response => response.text())
+    .then(data => {
+      console.log("ESP32 trả về:", data);
+      if (newState === "on") {
+        this.innerText = "Tắt Nguồn";
+      } else {
+        this.innerText = "Bật Nguồn";
+      }
+    })
+    .catch(error => {
+      console.error("Lỗi khi gửi yêu cầu:", error);
+    });
+});
+
+// Hàm xử lý nút led
+document.getElementById("btn-led").addEventListener("click", function(){
+  let currentText = this.innerText;
+  let newState = currentText.includes("Bật") ? "on" : "off";
+  fetch('/led?state=' + newState)
+    .then(response => response.text())
+    .then(data => {
+      console.log("ESP32 trả về:", data);
+      if (newState === "on") {
+        this.innerText = "Tắt Đèn";
+      } else {
+        this.innerText = "Bật Đèn";
+      }
+    })
+    .catch(error => {
+      console.error("Lỗi khi gửi yêu cầu:", error);
+    });
+});
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <mainserver.h>
+#include <Temp_Hum_Sensor.h>
 
 void TaskLEDControl(void *pvParameters) {
   pinMode(GPIO_NUM_48, OUTPUT); // Initialize LED pin
@@ -12,7 +13,7 @@ void TaskLEDControl(void *pvParameters) {
       digitalWrite(GPIO_NUM_48, LOW); // Turn OFF LED
     }
     ledState = 1 - ledState;
-    vTaskDelay(2000);
+    vTaskDelay(500);
   }
 }
 
@@ -21,10 +22,13 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   xTaskCreate(TaskLEDControl, "LED Control", 2048, NULL, 2, NULL);
+  xTaskCreate(Temp_Hum_Sensor, "Temp_Hum_Sensor", 2048, NULL, 2, NULL);
+  // xTaskCreate(setup_server, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
   xTaskCreate(main_server_task, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
 }
 
 void loop() {
-  Serial.println("Hello Custom Board");
+  // Serial.println("Hello Custom Board");
+  // loop_server();
   delay(1000);
 }
